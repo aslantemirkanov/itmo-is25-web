97919a55460064f38cc3dbf3d70bf95fe6d4fdf7
Aslan <aat0007@mail.ru>
2023-12-09 04:14:02 +0300

    lab-7

--- js/mousetrap.js
+++ js/mousetrap.js
@@ -1,15 +1,30 @@
+// Получаем все ссылки с классом '.head-navigation-panel__link'
 const links = document.querySelectorAll('.head-navigation-panel__link');
+
+// Начальный номер для горячих клавиш
 let hotkeyNumber = 1;
 
+// Проходим по каждой ссылке из навигационной панели
 links.forEach(function(link) {
+    // Создаем строку с горячей клавишей, например 'ctrl+alt+1', 'ctrl+alt+2', и т.д.
     const hotkey = 'ctrl+alt+' + hotkeyNumber;
 
+    // Привязываем горячую клавишу к действию
+    // hotkey - сочетание клавиш для активации
+    // функция - действие, которое будет выполнено при активации горячей клавиши
     Mousetrap.bind(hotkey, function() {
+        // Перенаправление на URL, указанный в атрибуте 'href' текущей ссылки
         window.location.href = link.getAttribute('href');
     });
+
+    // Увеличиваем номер для следующей горячей клавиши
     hotkeyNumber++;
 });
 
+// Базовая горячая клавиша для перенаправления на 'index.html'
+// 'ctrl+alt+0' - сочетание клавиш для активации
+// функция - действие, выполняемое при активации горячей клавиши
 Mousetrap.bind('ctrl+alt+0', function() {
+    // Перенаправление на 'index.html'
     window.location.href = 'index.html';
 });
